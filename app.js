1ad435975de8187014a977b7d2074d3cc1151b27
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-03-09 02:05:42 +0530

    sghdsga

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ User.hasMany(Diary)
 app.use(routes)
 sequelize.sync().then(result=>{
     
-    app.listen(process.env.PORT || 5000,()=>console.log("listening on 5000"))
+    app.listen(process.env.PORT || 8000,()=>console.log("listening on 5000"))
 }).catch(err=>{
     console.log(err)
 })
