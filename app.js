ed8ff23b976402863dcab0c9b59f9f7d845546c7
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-03-09 02:26:15 +0530

    brandh pidikunnu

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const sequelize=require('./util/database')
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
 var options = {
-	host: process.env.HOST,
+	host: "eu-cdbr-west-02.cleardb.net",
 	
-	user: process.env.USER,
-	password: process.env.PASSWORD,
-	database: process.env.DATABASE,
+	user: "b481f03d537d7e",
+	password: "2a5d63b0",
+	database: "heroku_8063a13ee0c730a",
     
 };
 
@@ -29,7 +29,8 @@ User.hasMany(Diary)
 app.use(routes)
 sequelize.sync().then(result=>{
     
-    app.listen(process.env.PORT || 8000,()=>console.log("listening on 5000"))
+    app.listen(process.env.PORT || 8000,()=>{
+		console.log("listening on 5000")})
 }).catch(err=>{
     console.log(err)
 })
--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-    host: process.env.HOST,
+const sequelize = new Sequelize("heroku_8063a13ee0c730a", "b481f03d537d7e", "2a5d63b0", {
+    host: "eu-cdbr-west-02.cleardb.net",
     dialect : 'mysql',
     
     
